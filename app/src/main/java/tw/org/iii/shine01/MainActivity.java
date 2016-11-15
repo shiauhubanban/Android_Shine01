af92eb18490b1a10b8b745e1fc788676b7733a3b
Liao <unidein@gmail.com>
2016-11-15 13:36:08 +0800

    20161115v2

--- app/src/main/java/tw/org/iii/shine01/MainActivity.java
+++ app/src/main/java/tw/org/iii/shine01/MainActivity.java
@@ -2,12 +2,36 @@ package tw.org.iii.shine01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
+    private View click;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        click = findViewById(R.id.click);
+        tv = (TextView) findViewById(R.id.tv);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              showLottery();
+            }
+        });
+    }
+    private void showLottery(){
+        HashSet<Integer> set = new HashSet<>();
+        while (set.size()<6) {
+            set.add((int) (Math.random() * 49 + 1));
+        }
+            tv.setText(set.toString());
+
     }
 }
